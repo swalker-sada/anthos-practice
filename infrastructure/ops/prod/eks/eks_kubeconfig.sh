6c4fc75aabcf3a883cb487aafee82faffcbadcb0
student_03_e5bf7ade12bb <student_03_e5bf7ade12bb@google.com>
2020-08-24 11:33:29 -0700

    cleanup

--- infrastructure/ops/prod/eks/eks_kubeconfig.sh
+++ infrastructure/ops/prod/eks/eks_kubeconfig.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 echo "Copying EKS clusters' kubeconfig files to GCS..."
+KUBECONFIG=kubeconfig_eks1:kubeconfig_eks2 kubectl config view --merge --flatten > config
 gsutil cp -r kubeconfig_eks1 gs://$PROJECT_ID/kubeconfig/kubeconfig_eks1
 gsutil cp -r kubeconfig_eks2 gs://$PROJECT_ID/kubeconfig/kubeconfig_eks2
-gsutil ls gs://$PROJECT_ID/kubeconfig
+gsutil cp -r kubeconfig_eks2 gs://$PROJECT_ID/kubeconfig/config
