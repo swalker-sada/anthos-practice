614af2837aaf192e2b6cf4431e1ba91df058c46d
student_02_19efbd29616f <student_02_19efbd29616f@google.com>
2020-08-24 20:35:29 -0700

    acm ssh key pairs

--- infrastructure/ops/prod/acm/main.tf
+++ infrastructure/ops/prod/acm/main.tf
@@ -4,3 +4,16 @@ resource "google_sourcerepo_repository" "acm_repo" {
   project = data.terraform_remote_state.vpc.outputs.project_id
 }
 
+resource "null_resource" "exec_prep_acm" {
+  provisioner "local-exec" {
+    interpreter = ["bash", "-exc"]
+    command     = "${path.module}/prep_acm.sh"
+    environment = {
+      PROJECT_ID         = data.terraform_remote_state.vpc.outputs.project_id
+    }
+  }
+
+  triggers = {
+    script_sha1          = sha1(file("prep_acm.sh"))
+  }
+}
