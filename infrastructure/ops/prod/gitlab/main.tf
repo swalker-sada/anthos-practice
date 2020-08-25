45eeed87838784f478370a3da81d1293ec900309
student_04_b326479b9993 <student_04_b326479b9993@google.com>
2020-08-25 14:40:26 -0700

    updated gitlab install

--- infrastructure/ops/prod/gitlab/main.tf
+++ infrastructure/ops/prod/gitlab/main.tf
@@ -1,9 +1,9 @@
 module "gke-gitlab" {
   source  = "terraform-google-modules/gke-gitlab/google"
 
-  project_id            = data.terraform_remote_state.vpc.outputs.project_id
+  project_id            = var.project_id
   domain                = "${trimprefix(module.cloud-endpoints-dns-gitlab.endpoint_computed, "gitlab.")}"
-  certmanager_email     = "no-reply@${data.terraform_remote_state.vpc.outputs.project_id}.example.com"
+  certmanager_email     = "no-reply@${var.project_id}.example.com"
   gitlab_runner_install = true
   gitlab_address_name   = google_compute_address.gitlab.name
   gitlab_db_name        = "gitlab-${lower(random_id.database_id.hex)}"
@@ -15,7 +15,7 @@ module "cloud-endpoints-dns-gitlab" {
   source  = "terraform-google-modules/endpoints-dns/google"
   version = "~> 2.0.1"
 
-  project     = data.terraform_remote_state.vpc.outputs.project_id
+  project     = var.project_id
   name        = "gitlab"
   external_ip = google_compute_address.gitlab.address
 }
@@ -24,13 +24,13 @@ module "cloud-endpoints-dns-registry" {
   source  = "terraform-google-modules/endpoints-dns/google"
   version = "~> 2.0.1"
 
-  project     = data.terraform_remote_state.vpc.outputs.project_id
+  project     = var.project_id
   name        = "registry"
   external_ip = google_compute_address.gitlab.address
 }
 
 resource "google_compute_address" "gitlab" {
-  project = data.terraform_remote_state.vpc.outputs.project_id
+  project = var.project_id
   region  = "us-central1"
   name    = "gitlab"
 }
