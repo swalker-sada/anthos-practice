debb37680bc1c2d6e1cf1daf1b3d98f59f625f06
Your Name <you@example.com>
2020-08-28 17:50:24 -0700

    fix ASM installer script

--- infrastructure/ops/prod/asm/get_svc_ingress_ip.sh
+++ infrastructure/ops/prod/asm/get_svc_ingress_ip.sh
@@ -5,6 +5,6 @@ export ingress=$(kubectl --context $1 -n istio-system get svc $2 -o json | jq -r
                 echo -e "Waiting for service $2 in cluster $1 to get an ILB IP..."
                 export ingress=$(kubectl --context $1 -n istio-system get svc $2 -o json | jq -r '.status.loadBalancer')
             done
-        export ingress_ip=$(kubectl --context $1 -n istio-system get svc $2 -o json | jq -r '.status.loadBalancer.ingress[].ip')
+        export ingress_ip=$(kubectl --context $1 -n istio-system get svc $2 -o json | jq -r '.status.loadBalancer.ingress[].hostname')
         echo -e "$2 in cluster $1 has an ILB IP of ${ingress_ip}."
 
--- infrastructure/ops/prod/asm/install_asm.sh
+++ infrastructure/ops/prod/asm/install_asm.sh
@@ -1,5 +1,43 @@
 #!/usr/bin/env bash
 
+
+is_deployment_ready() {
+kubectl --context $1 -n $2 get deploy $3 &> /dev/null
+    export exit_code=$?
+    while [ ! " ${exit_code} " -eq 0 ]
+        do 
+            sleep 5
+            echo -e "Waiting for deployment $3 in cluster $1 to be created..."
+            kubectl --context $1 -n $2 get deploy $3 &> /dev/null
+            export exit_code=$?
+        done
+    echo -e "Deployment $3 in cluster $1 created."
+
+    # Once deployment is created, check for deployment status.availableReplicas is greater than 0
+    export availableReplicas=$(kubectl --context $1 -n $2 get deploy $3 -o json | jq -r '.status.availableReplicas')
+    while [[ " ${availableReplicas} " == " null " ]]
+        do 
+            sleep 5
+            echo -e "Waiting for deployment $3 in cluster $1 to become ready..."
+            export availableReplicas=$(kubectl --context $1 -n $2 get deploy $3 -o json | jq -r '.status.availableReplicas')
+        done
+    
+    echo -e "$3 in cluster $1 is ready with replicas ${availableReplicas}."
+    return ${availableReplicas}
+}
+
+get_svc_ingress_ip() {
+export ingress=$(kubectl --context $1 -n istio-system get svc $2 -o json | jq -r '.status.loadBalancer')
+        while [[ " ${ingress} " == " {} " ]]
+            do 
+                sleep 5
+                echo -e "Waiting for service $2 in cluster $1 to get an ILB IP..."
+                export ingress=$(kubectl --context $1 -n istio-system get svc $2 -o json | jq -r '.status.loadBalancer')
+            done
+        export ingress_ip=$(kubectl --context $1 -n istio-system get svc $2 -o json | jq -r '.status.loadBalancer.ingress[].hostname')
+        echo -e "$2 in cluster $1 has an ILB IP of ${ingress_ip}."
+}
+
 # Define vars
 export ASM_DIR=istio-${ASM_VERSION}
 export GCP_NETWORK=gcp-vpc
@@ -23,10 +61,10 @@ kubectl config rename-context gke_${PROJECT_ID}_${GKE1_LOCATION}_${GKE1} ${GKE1}
 kubectl config rename-context gke_${PROJECT_ID}_${GKE2_LOCATION}_${GKE2} ${GKE2}
 
 # Wait until gatekeeper Pods are up
-./is_deployment_ready.sh ${GKE1} gatekeeper-system gatekeeper-controller-manager
-./is_deployment_ready.sh ${GKE2} gatekeeper-system gatekeeper-controller-manager
-./is_deployment_ready.sh ${EKS1} gatekeeper-system gatekeeper-controller-manager
-./is_deployment_ready.sh ${EKS2} gatekeeper-system gatekeeper-controller-manager
+is_deployment_ready.sh() ${GKE1} gatekeeper-system gatekeeper-controller-manager
+is_deployment_ready.sh() ${GKE2} gatekeeper-system gatekeeper-controller-manager
+is_deployment_ready.sh() ${EKS1} gatekeeper-system gatekeeper-controller-manager
+is_deployment_ready.sh() ${EKS2} gatekeeper-system gatekeeper-controller-manager
 
 # Create istio-system namespace and certs
 kubectl create namespace istio-system --dry-run -o yaml > istio-system.yaml
@@ -54,6 +92,9 @@ envsubst < eks2-cluster-asm.yaml_tmpl > eks2-cluster-asm.yaml
 # Deploy the istiooperator resource on the EKS clusters
 istioctl --context=${EKS1} manifest apply -f eks1-cluster-asm.yaml
 istioctl --context=${EKS2} manifest apply -f eks2-cluster-asm.yaml
+
+get_svc_ingress_ip() ${EKS1} istio-ingressgateway
+get_svc_ingress_ip() ${EKS2} istio-ingressgateway
 
 # Get the Istio ingress gateway IP addresses from both EKS clusters
 export EKS1_ISTIOINGRESS_HOSTNAME=$(kubectl --context ${EKS1} -n istio-system get svc istio-ingressgateway -ojson | jq -r '.status.loadBalancer.ingress[].hostname')
