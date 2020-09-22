2670e17031cc07d76514e0e165c2b10d16ce51f2
Kent Hua <40437-kenthua@users.noreply.gitlab.com>
2020-09-22 01:14:54 +0000

    formatting update

--- infrastructure/dev/gcp/mci/main.tf
+++ infrastructure/dev/gcp/mci/main.tf
@@ -1,5 +1,5 @@
 module "mci-dev"  {
-    source        = "../../../../platform_admins/shared_terraform_modules/gcp/mci/"
-    project_id    = data.terraform_remote_state.dev_gcp_vpc.outputs.project_id
-    cluster_name  = data.terraform_remote_state.dev_gcp_gke.outputs.gke_dev_1_name
+  source        = "../../../../platform_admins/shared_terraform_modules/gcp/mci/"
+  project_id    = data.terraform_remote_state.dev_gcp_vpc.outputs.project_id
+  cluster_name  = data.terraform_remote_state.dev_gcp_gke.outputs.gke_dev_1_name
 }
