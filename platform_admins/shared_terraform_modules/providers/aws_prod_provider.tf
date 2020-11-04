32eae9515ddbd6ec8b3382e55426df5932f65480
kenthua <kenthua@google.com>
2020-11-04 17:41:31 -0500

    re-add provider information

--- platform_admins/shared_terraform_modules/providers/aws_prod_provider.tf
+++ platform_admins/shared_terraform_modules/providers/aws_prod_provider.tf
@@ -14,3 +14,25 @@
  * limitations under the License.
  */
 
+variable "region" { default = "us-west-2" }
+
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = var.region
+}
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
