8d06b7e382ef692ec14ca36c580a399923f29b95
Kent Hua <kenthua@google.com>
2021-02-01 23:29:21 +0000

    bump helm to 4.8.2 for gitlab

--- platform_admins/shared_terraform_modules/gcp/gitlab/main.tf
+++ platform_admins/shared_terraform_modules/gcp/gitlab/main.tf
@@ -22,7 +22,7 @@ module "gke-gitlab" {
   certmanager_email     = "no-reply@${var.project_id}.example.com"
   gitlab_runner_install = true
   gitlab_db_name        = "gitlab-${lower(random_id.database_id.hex)}"
-  helm_chart_version    = "4.2.4"
+  helm_chart_version    = "4.8.2"
   gke_version           = "1.16"
 }
 
