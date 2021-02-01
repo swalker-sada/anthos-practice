ab56d5effe7c428cd06c3e4397f3d967cabd8c01
Kent Hua <kenthua@google.com>
2021-02-01 20:13:31 +0000

    comment out gitlab

--- platform_admins/shared_terraform_modules/gcp/gitlab/main.tf
+++ platform_admins/shared_terraform_modules/gcp/gitlab/main.tf
@@ -18,7 +18,7 @@ module "gke-gitlab" {
   source = "github.com/terraform-google-modules/terraform-google-gke-gitlab?ref=release-v0.4.1"
 
   project_id            = var.project_id
-  domain                = "trimprefix(module.cloud-endpoints-dns-gitlab.endpoint_computed, "gitlab.")"
+  domain                = trimprefix(module.cloud-endpoints-dns-gitlab.endpoint_computed, "gitlab.")
   certmanager_email     = "no-reply@${var.project_id}.example.com"
   gitlab_runner_install = true
 #  gitlab_address_name   = google_compute_address.gitlab.name
